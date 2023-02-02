21b95ebc431b4c3792ee4f58166f631ee4242ae6
Jacky Song <almey0721@gmail.com>
2023-02-02 03:44:53 -0500

    Added zeros() and ones()

--- src/tensor/mod.rs
+++ src/tensor/mod.rs
@@ -1,4 +1,4 @@
-use std::{fmt::Debug, ops::{IndexMut, Index}};
+use std::{fmt::Debug, ops::{IndexMut, Index, Add}, iter::Sum};
 
 // general tensor (multi-dimensional array) type
 #[derive(Debug)]
@@ -18,8 +18,40 @@ where
         }
     }
 
+    pub fn zeros(shape: [usize; N]) -> Self 
+    where
+        T: From<i32>
+    {
+        let mut i = 1;
+        for j in 0..shape.len() {
+            i *= shape[j]
+        }
+        Tensor {
+            shape,
+            data: vec![0.into(); i]
+        }
+    }
+
+    pub fn ones(shape: [usize; N]) -> Self 
+    where
+        T: From<i32>
+    {
+        let mut i = 1;
+        for j in 0..shape.len() {
+            i *= shape[j]
+        }
+        Tensor {
+            shape,
+            data: vec![1.into(); i]
+        }
+    }
+
     pub fn shape(&self) -> [usize; N] {
         self.shape
+    }
+
+    pub fn data(&self) -> &Vec<T> {
+        &self.data
     }
 
     pub fn len(&self) -> usize {
@@ -41,6 +73,25 @@ where
         }
         Ok(i)
     }
+
+    // fn add(&self, other: Tensor<T, N>) -> Result<Tensor<T, N>, String> 
+    //     where &'static T: Add<&'static T> + 'static
+    // {
+    //     if self.shape() != other.shape() {
+    //         return Err(format!("[elara-math] Cannot add tensors of different shapes {:?}, {:?}", self.shape(), other.shape()))
+    //     }
+    //     let first_data = self.data();
+    //     let second_data = other.data();
+    //     let mut sum_array = vec![0; self.len()];
+    //     for (i, (a, b)) in first_data.iter().zip(&second_data).enumerate() {
+    //         sum_array[i] = a + b;
+    //     }
+    //     let result = Tensor {
+    //         shape: self.shape,
+    //         data: sum_array
+    //     };
+    //     Ok(result)
+    // }
 }
 
     // Referenced https://codereview.stackexchange.com/questions/256345/n-dimensional-array-in-rust
