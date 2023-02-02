2e7c010b5581cb463a668aed0c95229ffa87d0e5
Jacky Song <almey0721@gmail.com>
2023-02-01 21:01:46 -0500

    Added indexing to multidim array

--- examples/demo.rs
+++ examples/demo.rs
@@ -1,7 +1,10 @@
 use elara_math::Tensor;
 
 fn main() {
-    let t = Tensor::new(&[1, 2, 3, 4, 5, 6], [2, 3]);
+    #[rustfmt::skip]
+    let t = Tensor::new(&[
+        1, 2, 3, 
+        4, 5, 6], [2, 3]);
 
-    println!("{:?}", t);
+    println!("{}", t[&[1, 0]]);
 }
--- src/tensor/mod.rs
+++ src/tensor/mod.rs
@@ -1,4 +1,4 @@
-use std::fmt::Debug;
+use std::{fmt::Debug, ops::{IndexMut, Index}};
 
 // general tensor (multi-dimensional array) type
 #[derive(Debug)]
@@ -28,5 +28,38 @@ where
 
     pub fn iter(&self) -> impl Iterator<Item = &T> {
         self.data.iter()
+    }
+
+    // Referenced https://codereview.stackexchange.com/questions/256345/n-dimensional-array-in-rust
+    fn get_index(&self, idx: &[usize; N]) -> Result<usize, String> {
+        let mut i = 0;
+        for j in 0..self.shape.len() {
+            if idx[j] >= self.shape[j] {
+                return Err(format!("[elara-math] Index {} is out of bounds for dimension {} with size {}", idx[j], j, self.shape[j]))
+            }
+            i = i * self.shape[j] + idx[j];
+        }
+        Ok(i)
+    }
+}
+
+    // Referenced https://codereview.stackexchange.com/questions/256345/n-dimensional-array-in-rust
+impl<T, const N: usize> Index<&[usize; N]> for Tensor<T, N> 
+where T: Clone
+{
+    type Output = T;
+
+    fn index(&self, idx: &[usize; N]) -> &T {
+        let i = self.get_index(&idx).unwrap();
+        &self.data[i]
+    }
+}
+
+impl<T, const N: usize> IndexMut<&[usize; N]> for Tensor<T, N> 
+where T: Clone
+{
+    fn index_mut(&mut self, idx: &[usize; N]) -> &mut T {
+        let i = self.get_index(idx).unwrap();
+        &mut self.data[i]
     }
 }
