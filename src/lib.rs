4863c52b09d2d93e07bf472bb69266954289ea02
Jacky Song <almey0721@gmail.com>
2023-02-19 17:27:34 -0500

    Added sigmoid and sigmoid_d functions to lib

--- src/lib.rs
+++ src/lib.rs
@@ -8,8 +8,38 @@ pub use tensor::*;
 
 use num_traits::Float;
 
-pub fn exp<T>(x: T) -> T 
+pub fn exp<T, const N: usize>(x: Tensor<T, N>) -> Tensor<T, N> 
 where T: Float
 {
-    x.exp()
+    let exp_vec = x.iter()
+        .map(|a| a.exp())
+        .collect();
+    Tensor {
+        shape: x.shape,
+        data: exp_vec
+    }
+}
+
+pub fn sigmoid<T, const N: usize>(x: Tensor<T, N>) -> Tensor<T, N>
+where T: Float
+{
+    let sigmoid_vec = x.iter()
+        .map(|a| T::one() / (T::one() + (-a.clone()).exp()))
+        .collect();
+    Tensor {
+        shape: x.shape,
+        data: sigmoid_vec
+    }
+}
+
+pub fn sigmoid_d<T, const N: usize>(x: Tensor<T, N>) -> Tensor<T, N>
+where T: Float
+{
+    let sigmoid_vec = x.iter()
+        .map(|a| a.clone() * (T::one() - a.clone()))
+        .collect();
+    Tensor {
+        shape: x.shape,
+        data: sigmoid_vec
+    }
 }
