ea159da1176651ffab7d2ee43b7887a9cb064f94
Jacky Song <almey0721@gmail.com>
2023-02-20 08:55:59 -0500

    Make most ops take tensors by reference

--- examples/relu.rs
+++ examples/relu.rs
@@ -3,6 +3,6 @@ use elara_math::{Tensor, relu};
 fn main() {
     let t1: Tensor<i32, 2> = Tensor::arange(-10..10).reshape([2, 5]);
     println!("{:?}", t1);
-    let t2 = relu(t1);
+    let t2 = relu(&t1);
     println!("{:?}", t2);
 }
--- examples/tiny_nn.rs
+++ examples/tiny_nn.rs
@@ -18,14 +18,14 @@ fn main() {
     let mut weights = Tensor::random([3, 1]) * 2.0 - 1.0;
     println!("Weights before training: {:?}", weights);
     for _ in 0..EPOCHS {
-        let output = sigmoid(train_data.matmul(&weights));
+        let output = sigmoid(&train_data.matmul(&weights));
         let error = &train_labels - &output;
-        let m = error * sigmoid_d(output);
+        let m = error * sigmoid_d(&output);
         let adjustment = train_data.t().matmul(&m);
         weights = weights + adjustment;
     }
     let pred_data: Tensor<f64, 2> = Tensor::new(&[1.0, 0.0, 0.0], [1, 3]);
-    let pred = sigmoid(pred_data.matmul(&weights));
+    let pred = sigmoid(&pred_data.matmul(&weights));
     println!("Weights after training: {:?}", weights);
     println!("Prediction [1, 0, 0] -> {:?}", pred.data[0]);
 }
