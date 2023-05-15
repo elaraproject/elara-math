8c235d24b809208a4b21cc38b13cb14e69c7647d
Jacky Song <almey0721@gmail.com>
2023-05-15 08:57:42 -0400

    Use tensor! for matmul example

--- examples/matmul.rs
+++ examples/matmul.rs
@@ -1,12 +1,14 @@
-use elara_math::Tensor;
+use elara_math::tensor;
 
 fn main() {
-    let t1: Tensor<f64, 2> = Tensor::new(&[
-    	1.0, 2.0,
-    	3.0, 4.0], [2, 2]);
-    let t2: Tensor<f64, 2> = Tensor::new(&[
-    	5.0, 6.0,
-    	7.0, 8.0], [2, 2]);
+    let t1 = tensor![
+        [1.0, 2.0],
+        [3.0, 4.0]
+    ];
+    let t2 = tensor![
+        [5.0, 6.0],
+        [7.0, 8.0]
+    ];
     println!("{:?}", t1);
     println!("{:?}", t2);
     // Expected: [[19, 22], [43, 50]]
