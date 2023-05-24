fd788dd1c3c417ab718761301d381f206fcbfe6f
Jacky Song <almey0721@gmail.com>
2023-05-24 14:15:15 -0400

    Use new array! syntax

--- examples/matmul.rs
+++ examples/matmul.rs
@@ -1,11 +1,11 @@
-use elara_math::tensor;
+use elara_math::array;
 
 fn main() {
-    let t1 = tensor![
+    let t1 = array![
         [1.0, 2.0],
         [3.0, 4.0]
     ];
-    let t2 = tensor![
+    let t2 = array![
         [5.0, 6.0],
         [7.0, 8.0]
     ];
