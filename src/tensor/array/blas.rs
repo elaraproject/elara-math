7c27a47890ce4fc3d23f32ae4b7541b91153367b
Jacky Song <almey0721@gmail.com>
2023-06-09 10:17:12 -0400

    Revert back to regular matmul as transpose optimization is broken

--- src/tensor/array/blas.rs
+++ src/tensor/array/blas.rs
@@ -11,22 +11,18 @@ pub fn transpose(a: &[f64], m: usize, n: usize) -> Vec<f64> {
     b
 }
 
-/// Optimized matrix multiplication
+/// Matrix multiplication
 /// Performs AB -> C
 /// where A dims = N x M
 /// and B dims = M x P
 /// and C dims N x P
 pub fn dgemm(n: usize, m: usize, p: usize, a: &[f64], b: &[f64]) -> Vec<f64> {
+    // TODO: optimize this or replace with actual BLAS
     let mut c = vec![0.0; n * p];
-
-    // Matrix multiply by transpose
-    // to speed up performance
-    let b_transpose = transpose(&b, m, p);
-
     for i in 0..n {
         for j in 0..p {
             for k in 0..m {
-                c[(p * i) + j] += a[(m * i) + k] * b_transpose[(p * j) + k];
+                c[(p * i) + j] += a[(m * i) + k] * b[(p * k) + j];
             }
         }
     }
