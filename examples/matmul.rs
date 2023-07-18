d8fae04b28beed4ac4e14bd98c0e02b3e749a748
Jacky Song <almey0721@gmail.com>
2023-07-18 08:23:48 +0800

    Update matmul example

--- examples/matmul.rs
+++ examples/matmul.rs
@@ -1,8 +1,8 @@
-use elara_math::array;
+use elara_math::prelude::*;
 
 fn main() {
-    let t1 = array![[1.0, 2.0], [3.0, 4.0]];
-    let t2 = array![[5.0, 6.0], [7.0, 8.0]];
+    let t1 = tensor![[1.0, 2.0], [3.0, 4.0]];
+    let t2 = tensor![[5.0, 6.0], [7.0, 8.0]];
     println!("{:?}", t1);
     println!("{:?}", t2);
     // Expected: [[19, 22], [43, 50]]
