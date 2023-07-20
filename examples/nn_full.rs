8988e64949f2cbccc0c38e4e4e147fa423e20230
Jacky Song <almey0721@gmail.com>
2023-07-20 12:43:20 +0800

    Updated NN full example

--- examples/nn_full.rs
+++ examples/nn_full.rs
@@ -1,18 +1,19 @@
 use elara_math::prelude::*;
+use elara_log::prelude::*;
 
 fn main() {
-    let x = Tensor::arange(-5..5, [10, 1]);
+    Logger::new().init().unwrap();
+    let x = Tensor::linspace(-5.0, 5.0, 50);
     let func = &x * -5.0;
-    let y = &func + &(Tensor::rand([10, 1]) * 0.4);
-    println!("Train X:\n{:?}", x);
-    println!("Train Y:\n{:?}", y);
+    let y = &func + &(Tensor::rand([50, 1]) * 0.4);
 
     let mut model = Model::new();
-    model.add_layer(Linear::new(1, 10, Activations::ReLU));
-    model.add_layer(Linear::new(10, 10, Activations::ReLU));
+    model.add_layer(Linear::new(1, 10, Activations::None));
+    model.add_layer(Linear::new(10, 10, Activations::None));
     model.add_layer(Linear::new(10, 1, Activations::None));
 
-    model.fit(&x, &y, 1000, 0.001, false);
+    model.compile(Optimizers::BGD);
+    model.fit(&x, &y, 2000, 0.001, true);
 
     let x_pred = scalar!(1.0);
     let y_pred = model.predict(&x_pred);
