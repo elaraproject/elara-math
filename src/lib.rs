131d1357e788c157ab0b3f3986cffe07b012f415
Jacky Song <jacky.song.elara@gmail.com>
2025-04-11 04:05:25 -0400

    Slightly tweaked wording in documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
-/*! This crate is a Rust-native tensor and math library, 
-developed for [Project Elara](https://github.com/elaraproject), that eventually aims to
+/*! This crate is a Rust-native tensor and math library,
+developed for [Project Elara](https://github.com/elaraproject). It aims to
 be a building block of a comprehensive machine learing and scientific computing
-library in Rust. It tries to be the analogue of [SciPy](https://scipy.org/),
-[NumPy](https://numpy.org/), and [PyTorch](https://pytorch.org/),
+library in Rust. It tries to give a Rust experience similar to [SciPy](https://scipy.org/),
+[NumPy](https://numpy.org/), and [PyTorch](https://pytorch.org/)/[TensorFlow](https://www.tensorflow.org/),
 offering:
 
 - Tensors: N-dimensional differentiable arrays (via `Tensor`)
