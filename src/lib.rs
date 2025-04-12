9011184cdffa13ffd0ec7f030fbf8eda90554b70
Jacky Song <jacky.song.elara@gmail.com>
2025-04-12 18:28:38 -0400

    Some small documentation improvements

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,15 @@
 /*! This crate is a Rust-native tensor and math library,
 developed for [Project Elara](https://github.com/elaraproject). It aims to
-be a building block of a comprehensive machine learing and scientific computing
-library in Rust. It tries to give a Rust experience similar to [SciPy](https://scipy.org/),
+be a basic implementation of common machine learing and scientific computing
+tools in Rust. It tries to give a Rust experience similar to [SciPy](https://scipy.org/),
 [NumPy](https://numpy.org/), and [PyTorch](https://pytorch.org/)/[TensorFlow](https://www.tensorflow.org/),
 offering:
 
 - Tensors: N-dimensional differentiable arrays (via `Tensor`)
-- An implementation of reverse-mode autodifferentiation
-- Numerical solvers for calculus (only numerical integral evaluation is fully-supported at the moment, the ODE solver has been moved to [`elara-array`](https://github.com/elaraproject/elara-array)):
+- An implementation of reverse-mode automatic differentiation
+- Numerical solvers for calculus (only numerical integration/quadrature is fully-supported at the moment, the ODE solver has been moved to [`elara-array`](https://github.com/elaraproject/elara-array))
+
+To get started, just run `cargo install elara-math` to add to your project. We offer [several examples](https://github.com/elaraproject/elara-math/tree/main/examples) in our GitHub repository to reference and learn from.
 */
 
 mod integrate;
